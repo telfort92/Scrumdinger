22cd761377907d0673e7a5964d1a8a1de101e792
telfort92 <telfortps@outlook.com>
2024-05-08 20:56:53 -0500

    Modified the project to pass data from scrum sheet and to log history

---
+++ Scrumdinger/Models/History.swift
@@ -0,0 +1,20 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by pitel on 5/8/24.
+//
+
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee]) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+    }
+}
